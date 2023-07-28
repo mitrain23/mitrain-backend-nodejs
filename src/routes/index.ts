61589782cc381c41897b27e60c41cdbbae78e1b8
mhikmal03 <hikmalkp@gmail.com>
2023-07-28 13:55:10 +0700

    tes2

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,13 @@ import postRouter from './postRoute';
 
 
 
-
+const router = express.Router();
 const allRoutes = express();
 
 allRoutes.use('/api', userRouter)
 allRoutes.use('/api', postRouter)
+router.get('/hello', (req, res) => {
+    res.send('Hello, World!');
+  });
 
 export default allRoutes
